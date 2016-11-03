ca569f331be0744890f5e098d23ba2f13a57d5a5
Justin <justincyork@gmail.com>
2016-11-03 18:13:35 +0000

    better ordinances test

--- test/data/ordinances.js
+++ test/data/ordinances.js
@@ -1,30 +1,76 @@
 module.exports = {
   "persons" : [ {
-    "id" : "LJKR-MNY",
+    "id" : "LCXV-8BT",
     "links" : {
       "person" : {
-        "href" : "https://familysearch.org/platform/tree/persons/LJKR-MNY"
+        "href" : "https://familysearch.org/platform/tree/persons/LCXV-8BT"
       }
     },
     "ordinances" : [ {
       "type" : "http://lds.org/Baptism",
-      "status" : "http://familysearch.org/v1/Ready"
+      "status" : "http://familysearch.org/v1/Completed",
+      "date" : {
+        "formal" : "+2011-12-21"
+      },
+      "templeCode" : "NZEAL"
     }, {
       "type" : "http://lds.org/Confirmation",
-      "status" : "http://familysearch.org/v1/Ready"
+      "status" : "http://familysearch.org/v1/Completed",
+      "date" : {
+        "formal" : "+2011-12-21"
+      },
+      "templeCode" : "NZEAL"
     }, {
       "type" : "http://lds.org/Initiatory",
-      "status" : "http://familysearch.org/v1/Ready"
+      "status" : "http://familysearch.org/v1/Completed",
+      "date" : {
+        "formal" : "+2011-12-28"
+      },
+      "templeCode" : "NZEAL"
     }, {
       "type" : "http://lds.org/Endowment",
-      "status" : "http://familysearch.org/v1/Ready"
+      "status" : "http://familysearch.org/v1/Completed",
+      "date" : {
+        "formal" : "+2013-11-08"
+      },
+      "templeCode" : "JRIVE"
     }, {
       "type" : "http://lds.org/SealingToSpouse",
-      "status" : "http://familysearch.org/v1/Ready",
+      "status" : "http://familysearch.org/v1/Completed",
       "spouse" : {
-        "resource" : "https://familysearch.org/platform/tree/persons/LJKR-MVH",
-        "resourceId" : "LJKR-MVH"
-      }
+        "resource" : "https://familysearch.org/platform/tree/persons/LCXV-DST",
+        "resourceId" : "LCXV-DST"
+      },
+      "date" : {
+        "formal" : "+2013-11-14"
+      },
+      "templeCode" : "DRAPE"
+    }, {
+      "type" : "http://lds.org/SealingToSpouse",
+      "status" : "http://familysearch.org/v1/Completed",
+      "spouse" : {
+        "resource" : "https://familysearch.org/platform/tree/persons/LZPP-NTT",
+        "resourceId" : "LZPP-NTT"
+      },
+      "date" : {
+        "formal" : "+2015-05-23"
+      },
+      "templeCode" : "KANSA"
+    }, {
+      "type" : "http://lds.org/SealingChildToParents",
+      "status" : "http://familysearch.org/v1/Completed",
+      "father" : {
+        "resource" : "https://familysearch.org/platform/tree/persons/LDJP-635",
+        "resourceId" : "LDJP-635"
+      },
+      "mother" : {
+        "resource" : "https://familysearch.org/platform/tree/persons/LDJP-6WB",
+        "resourceId" : "LDJP-6WB"
+      },
+      "date" : {
+        "formal" : "+2016-01-28"
+      },
+      "templeCode" : "MTIMP"
     } ]
   } ]
 };
